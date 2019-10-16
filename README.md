77fd8770959fe0fb154aa2b481587b4b2c24b892
vsaveris <vsaveris@gmail.com>
2019-10-17 00:59:02 +0200

    ref: Parse input parameters in demoga.py
    Demonstration algorithm is passed as an input parameter to the demoga.py
    script. README.md updated to reflect the updates.

--- README.md
+++ README.md
@@ -1,7 +1,32 @@
 # graph-algorithms
-Implementation in python of graph algorithms (*reference: Introduction to Algorithms, 3rd Edition, Chapter VI*)
-* **Breadth-first search (BFS)** (*additional reference: DPV Algorithms, Chapter 4.3*): BFS algorithm is an algorithm for finding the shortest paths in any graph G = (V,E) whose edges have unit length. The time complexity of the algorithm is O(|V|+|E|).
-* **Dijkstra's Algorithm** (*additional reference: DPV Algorithms, Chapter 4.4*): Dijkstra's algorithm is an algorithm for finding the shortest paths in any graph G = (V,E) whose edges lengths are positive numbers. The time complexity of the algorithm is O((|V|+|E|)log|V|), when using a priority queue.
+## Description
+Implementation of graph algorithms in python (file: `graphalgorithms.py`)
+* **Breadth-first search (BFS)**: BFS algorithm is an algorithm for finding the shortest paths in any graph G = (V,E) whose edges have unit length. The time complexity of the algorithm is O(|V|+|E|).
+* **Dijkstra's Algorithm**: Dijkstra's algorithm is an algorithm for finding the shortest paths in any graph G = (V,E) whose edges lengths are positive numbers. The time complexity of the algorithm is O((|V|+|E|)log|V|), when using a priority queue.
 
-**Prerequisites**
+## Demonstration
+Inside the `demo` folder there is a demonstration script (`demoga.py`), including usage examples for the `GraphAlgorithms` class.
+```
+$python demoga.py -h
+usage: demoga.py [-h] -a algorithm
+
+Demonstration script for the GraphAlgorithms class
+
+optional arguments:
+  -h, --help    show this help message and exit
+  -a algorithm  demonstration algorithm
+
+Supported values for 'algorithm' are ('bfs','dijkstra')
+
+Example:
+python demoga.py -a dijkstra
+```
+
+## Prerequisites
 1. [python 3.6](https://www.python.org/downloads/release/python-369/)
+
+## References
+1. *Introduction to Algorithms, 3rd Edition, Chapter VI, Graph algorithms*
+2. *DPV Algorithms, Chapter 3, Decompositions of graphs*
+3. *DPV Algorithms, Chapter 4.4, Paths in graphs*
+
--- demo/demoga.py
+++ demo/demoga.py
@@ -12,70 +12,95 @@ Date last modified: 16.10.2019
 Python Version: 3.6
 '''
 
-import sys
+import sys, argparse
 sys.path.insert(1, '../')
 import graphalgorithms as ga
 
-'''
-Example code for the Dijkstra's algorithm.
-'''
-# Define the vertices and edges of the graph, in the structure 
-# the class Graph expects them
-vertices = ['a', 'b', 'c', 'd', 'e', 'f', 's', 't']   
-edges_with_lengths = {'s': [('a', 100), ('c', 90), ('e', 101)], 
-                      'a': [('b', 100)],
-                      'b': [('t', 5)],
-                      'c': [('d', 105)],
-                      'd': [('t', 10)],
-                      'e': [('f', 101)],
-                      'f': [('t', 1)]}
 
-# Create the graph object G = (V, E) given the V and E
-g = ga.Graph(vertices, edges_with_lengths)
+def demoDijkstra():
+    '''
+    Example code for the Dijkstra's algorithm.
+    '''
+    # Define the vertices and edges of the graph, in the structure 
+    # the class Graph expects them
+    vertices = ['a', 'b', 'c', 'd', 'e', 'f', 's', 't']   
+    edges_with_lengths = {'s': [('a', 100), ('c', 90), ('e', 101)], 
+                          'a': [('b', 100)],
+                          'b': [('t', 5)],
+                          'c': [('d', 105)],
+                          'd': [('t', 10)],
+                          'e': [('f', 101)],
+                          'f': [('t', 1)]}
 
-# Run Dijkstra's algorithm for the defined graph
-paths, cost = ga.GraphAlgorithms().dijkstra(G = (g.getVertices(), g.getEdges()), l = g.getLengths(), s = 's')
+    # Create the graph object G = (V, E) given the V and E
+    g = ga.Graph(vertices, edges_with_lengths)
+    
+    # Run Dijkstra's algorithm for the defined graph
+    paths, cost = ga.GraphAlgorithms().dijkstra(G = (g.getVertices(), g.getEdges()), l = g.getLengths(), s = 's')
+    
+    # Back tracking the paths, we can get the shortest path for any destination node
+    # Example: Get the shortest path from 's' to 't'
+    done = False
+    shortest_path = ['t']
+    current = 't'
+    while not done:
+        shortest_path.insert(0, paths[current])       
+        current = shortest_path[0]
+        if current == 's':
+            done = True
+    
+    print('Dijkstra: Shortest path from \'s\' to \'t\' is: ', shortest_path, ' with cost ', cost['t'], sep = '')
+    
 
-# Back tracking the paths, we can get the shortest path for any destination node
-# Example: Get the shortest path from 's' to 't'
-done = False
-shortest_path = ['t']
-current = 't'
-while not done:
-    shortest_path.insert(0, paths[current])       
-    current = shortest_path[0]
-    if current == 's':
-        done = True
+def demoBfs():
+    '''
+    Example code for the BFS algorithm.
+    '''
+    # Define the vertices and edges of the graph, in the structure 
+    # the class Graph expects them
+    vertices = ['a', 'b', 'c', 'd', 'e', 'f', 's', 't']   
+    edges_with_lengths = {'s': [('a', 1), ('d', 1), ('e', 1)], 
+                          'a': [('b', 1)],
+                          'b': [('c', 1)],
+                          'c': [('t', 1)],
+                          'd': [('t', 1)],
+                          'e': [('f', 1)],
+                          'f': [('t', 1)]}
+    
+    # Create the graph object G = (V, E) given the V and E
+    g = ga.Graph(vertices, edges_with_lengths)
+    
+    # Run BFS algorithm for the defined graph
+    paths, cost = ga.GraphAlgorithms().bfs(G = (g.getVertices(), g.getEdges()), s = 's')
+    
+    done = False
+    shortest_path = ['t']
+    current = 't'
+    while not done:
+        shortest_path.insert(0, paths[current])       
+        current = shortest_path[0]
+        if current == 's':
+            done = True
+    
+    print('BFS: Shortest path from \'s\' to \'t\' is: ', shortest_path, ' with cost ', cost['t'], sep = '')
 
-print('Dijkstra: Shortest path from \'s\' to \'t\' is: ', shortest_path, ' with cost ', cost['t'], sep = '')
 
-'''
-Example code for the BFS algorithm.
-'''
-# Define the vertices and edges of the graph, in the structure 
-# the class Graph expects them
-vertices = ['a', 'b', 'c', 'd', 'e', 'f', 's', 't']   
-edges_with_lengths = {'s': [('a', 1), ('d', 1), ('e', 1)], 
-                      'a': [('b', 1)],
-                      'b': [('c', 1)],
-                      'c': [('t', 1)],
-                      'd': [('t', 1)],
-                      'e': [('f', 1)],
-                      'f': [('t', 1)]}
+if __name__ == '__main__':
 
-# Create the graph object G = (V, E) given the V and E
-g = ga.Graph(vertices, edges_with_lengths)
-
-# Run BFS algorithm for the defined graph
-paths, cost = ga.GraphAlgorithms().bfs(G = (g.getVertices(), g.getEdges()), s = 's')
-
-done = False
-shortest_path = ['t']
-current = 't'
-while not done:
-    shortest_path.insert(0, paths[current])       
-    current = shortest_path[0]
-    if current == 's':
-        done = True
-
-print('BFS: Shortest path from \'s\' to \'t\' is: ', shortest_path, ' with cost ', cost['t'], sep = '')
+    # Parsing input arguments
+    description_message = 'Demonstration script for the GraphAlgorithms class'
+    epilog_message = 'Supported values for \'algorithm\' are (\'bfs\',\'dijkstra\')\n\n' +\
+                    'Example: \npython demoga.py -a dijkstra'
+    args_parser = argparse.ArgumentParser(description = description_message, epilog = epilog_message,
+                formatter_class=argparse.RawTextHelpFormatter)
+    args_parser.add_argument('-a', action = 'store', required = True, help = 'demonstration algorithm',
+                            choices = ('bfs', 'dijkstra'), metavar = 'algorithm')
+    args = args_parser.parse_args()
+      
+    # Execute the requested demonstration code
+    if args.a == 'bfs':
+        demoBfs()
+    elif args.a == 'dijkstra':
+        demoDijkstra()
+    else:
+        print('Demonstration code for algorithm \'', args.a, '\' is missing', sep = '')
